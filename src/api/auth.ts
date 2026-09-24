81e1639ea8e8b1c20633f08ea2e12063d0605a45
Abhishek kalme <abhishekkalme0@gmail.com>
2026-09-24 18:04:56 +0530

    Set up shared Axios API layer

---
+++ src/api/auth.ts
@@ -0,0 +1,22 @@
+import api from "./axios";
+
+export interface LoginData {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  id: number;
+  username: string;
+  email: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const loginUser = async (
+  data: LoginData
+): Promise<LoginResponse> => {
+  const response = await api.post<LoginResponse>("/auth/login", data);
+
+  return response.data;
+};
